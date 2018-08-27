53414609af605a14828927ed40c46fd51bdeb384
Zhuyuqi <396838319@qq.com>
2018-08-27 17:57:09 +0800

    开发datepicker组件

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-Vue.component('k-dialog', k_dialog)
+import Vue from 'vue'
+import k_datepicker from './components/datepicker.vue'
+// Vue.component('k-dialog', k_dialog)
+Vue.component('k-datepicker', k_datepicker)
 var app = new Vue({
   el: '#app',
   data: {
