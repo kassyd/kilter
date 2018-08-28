f5f786ed97fd7b66a3caaf9dc80b0b43adf81ba6
Zhuyuqi <396838319@qq.com>
2018-08-28 18:04:06 +0800

    测试datepicker组件

---
+++ components/demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <ul class="content">
+      <li>....</li>
+      <li>.....</li>
+      <li>......</li>
+      <li>...</li>
+      <li>...</li>
+      <li>...</li>
+      <li>......</li>
+      <li>...</li>
+      <li>......</li>
+      <li>...</li>
+      <li>...</li>
+    </ul>
+  </div>
+</template>
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    mounted() {
+      this.$nextTick(() => {
+        this.scroll = new BScroll(this.$refs.wrapper, {})
+      })
+    }
+  }
+</script>
+<style>
+.wrapper {
+  height: 50px;
+  background-color: red;
+  overflow: scroll;
+}
+</style>
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
-import k_datepicker from './components/datepicker.vue'
+// import k_datepicker from './components/datepicker.vue'
+import k_datepicker from './components/datepickerDemo.vue'
+// import demo from './components/demo.vue'
 // Vue.component('k-dialog', k_dialog)
 Vue.component('k-datepicker', k_datepicker)
+// Vue.component('demo', demo)
 var app = new Vue({
   el: '#app',
   data: {
@@ -13,6 +16,10 @@ var app = new Vue({
     },
     open() {
       this.isShow = true
+    },
+    haha(hierarchy, texts) {
+      console.log(1)
+      console.log(hierarchy, texts)
     }
   }
 })
