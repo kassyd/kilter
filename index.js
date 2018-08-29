9130cb5d72eaedba702b4bdc123b3291da60183c
Zhuyuqi <396838319@qq.com>
2018-08-29 15:21:10 +0800

    测试datepicker单文件

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 // import k_datepicker from './components/datepicker.vue'
-import k_datepicker from './components/datepickerDemo.vue'
+// import k_datepicker from './components/datepickerDemo.vue'
 // import demo from './components/demo.vue'
 // Vue.component('k-dialog', k_dialog)
 Vue.component('k-datepicker', k_datepicker)
@@ -8,18 +8,22 @@ Vue.component('k-datepicker', k_datepicker)
 var app = new Vue({
   el: '#app',
   data: {
-    isShow: true
+    isShow: true,
+    datepickerShow: false
   },
   methods: {
     close() {
       this.isShow = false
+      this.datepickerShow = false
     },
     open() {
       this.isShow = true
+      this.datepickerShow = true
     },
-    haha(hierarchy, texts) {
+    sureClick(hierarchy, texts) {
       console.log(1)
       console.log(hierarchy, texts)
+      this.datepickerShow = false
     }
   }
 })
